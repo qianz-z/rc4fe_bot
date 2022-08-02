d30858772c1c7bbb47bcd903451354d13c51a1f6
qianz-z <qianz321@gmail.com>
2022-08-02 22:11:22 +0800

    register function with all text inputs

--- main.py
+++ main.py
@@ -22,6 +22,10 @@ from keys import API_KEY, USER_ADMINS
 _STATES = [
     'USER_MENU',
     'ADMIN_MENU',
+    'USER_REGISTER_NAME',
+    'USER_REGISTER_MOBILE',
+    'USER_REGISTER_HOUSE',
+    'USER_REGISTER_ROOM',
 ]
 
 # Set some state variables
@@ -44,6 +48,10 @@ def start(update, context):
         # RESIDENTS
         text = "Hello! I am RC4fe Telegram bot, called Bob!"
         update.message.reply_text(text)
+        context.user_data["name"] = None
+        context.user_data["mobile"] = None
+        context.user_data["house"] = None
+        context.user_data["room"] = None
         print("RC4fe Bob is alive!")
         return user_menu(update, context)
     else:
@@ -51,19 +59,72 @@ def start(update, context):
         update.message.reply_text(text)
         return
 
+# ====================  USER FUNCTIONS ====================
+
 def user_menu(update, context):
-    keyboard = [
-        [InlineKeyboardButton("Order", callback_data='user_order')],
-        [InlineKeyboardButton("Remind", callback_data='user_remind')],
-    ]
+    keyboard = []
+    if not context.user_data['name']:
+        keyboard.append([InlineKeyboardButton("Register", callback_data='user_register')]),
+    
+    keyboard.append([InlineKeyboardButton("Order", callback_data='user_order')])
+    keyboard.append([InlineKeyboardButton("Remind", callback_data='user_remind')])
+    
 
     text = "What would you like Bob to do?"
     update.message.reply_text(text, reply_markup=InlineKeyboardMarkup(keyboard))
     return USER_MENU
 
+def user_register(update, context):
+    update.callback_query.delete_message()
+    text = "Hello! My Name is Bob and I am here to serve your needs for admin stuffs related to RC4fe. \n"
+    text += "Before we start, please enter your name so that I can remember your existence"
+    update.callback_query.message.chat.send_message(text)
+    return USER_REGISTER_NAME
+
+def user_register_name(update, context):
+    user_name = update.message.text
+    context.user_data['name'] = user_name
+
+    text = f"Hello {user_name}!! What a nice name! Please share with me your mobile number so that I can contact you in the future."
+    update.message.reply_text(text)
+    return USER_REGISTER_MOBILE
+
+def user_register_mobile(update, context):
+    user_mobile = update.message.text
+    context.user_data['mobile'] = user_mobile
+
+    user_name = context.user_data['name']
+    text = f"{user_name}, with contact number of {user_mobile} will be saved in my brain's memory. \n"
+    text += "Which is the cool house that you are in?"
+    update.message.reply_text(text)
+    return USER_REGISTER_HOUSE
+
+def user_register_house(update, context):
+    user_house = update.message.text
+    context.user_data['house'] = user_house
+
+    text = f"{user_house} best house! \n"
+    text += "Last question, what is your room number?"
+    update.message.reply_text(text)
+    return USER_REGISTER_ROOM
+
+def user_register_room(update, context):
+    user_room = update.message.text
+    context.user_data['room'] = user_room
+
+    user_name = context.user_data['name']
+    user_mobile = context.user_data['mobile']
+    user_house = context.user_data['house']
+    text = f"{user_name} from {user_house}, living in {user_room} and with contact number {user_mobile}. \n"
+    text += "Thanks for the free stalking information! Just joking! Your details will be protected and will not be shared around. \n"
+    text += "If you would like to edit your profile details, you can go to the /user_menu and click on the 'Edit Profile' button."
+    update.message.reply_text(text)
+    return USER_MENU
+
+
 def user_order(update, context):
     update.callback_query.delete_message()
-    text = "USER ORDER"
+    text = "Bob will now take the order for you..."
     print(text)
     update.callback_query.message.chat.send_message(text)
     return
@@ -96,11 +157,24 @@ if __name__ == '__main__':
         states={
              # current status: whatisreceivedfromuser(function to be called, pattern  = 'callback_data')
             USER_MENU: [
+                CallbackQueryHandler(user_register, pattern='user_register'),
                 CallbackQueryHandler(user_order, pattern='user_order'),
                 CallbackQueryHandler(user_remind, pattern='user_remind'),
             ],
             ADMIN_MENU: [
                 CallbackQueryHandler(admin_order, pattern='admin_order'),
+            ],
+            USER_REGISTER_NAME: [
+                MessageHandler(Filters.text, callback=user_register_name),
+            ],
+            USER_REGISTER_MOBILE:[
+                MessageHandler(Filters.text, callback=user_register_mobile),
+            ],
+            USER_REGISTER_HOUSE:[
+                MessageHandler(Filters.text, callback=user_register_house),
+            ],
+            USER_REGISTER_ROOM:[
+                MessageHandler(Filters.text, callback=user_register_room),
             ],
         },
 
