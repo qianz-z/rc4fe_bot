e6841aa0c1b4189549fc44f878f055acf09b1f23
qian <qianz321@gmail.com>
2022-08-11 21:23:00 +0800

    remove tempcoderunnerfile

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-_data["total_num_of_orders"] = 1
-    dispatcher.bot_data["orders"].append({
-        "order_index": 1,
-        'name': "Qian",
-        'desc': "this",
-        'price': 6,
-        'photo': None,
-    })
